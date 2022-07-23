479a0847cf8be9c1c31f6547028cc31a025ad31a
tomatjgsu <chase_dream_45@163.com>
2022-07-23 16:57:25 +0800

    第二次提交

--- src/main/java/com/jgsu/demo/HelloDemo.java
+++ src/main/java/com/jgsu/demo/HelloDemo.java
@@ -2,5 +2,6 @@ package com.jgsu.demo;
 public class HelloDemo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("第一次修改");
     }
 }
